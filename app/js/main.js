f394ed9c75d3938c8e5cb63ca6865f4e80509240
HawkeyeDZN <pashagoncharov2015@gmail.com>
2022-03-22 00:28:56 +0300

    added shop page

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,39 @@
 $(function(){
+
+  $('.shop-content__filter-btn').on('click', function() {
+    // $(this).addClass('shop-content__filter-btn--active')
+    $('.shop-content__filter-btn').toggleClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product-item').addClass('product-item--list');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product-item').removeClass('product-item--list');
+  });
+
+  $('.select-style').styler();
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "Br ",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  })
   
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
-    autoplaySpeed: 3000,
-    touchMove:false
+    autoplaySpeed: 3000
   });
 
   $(".star").rateYo({
